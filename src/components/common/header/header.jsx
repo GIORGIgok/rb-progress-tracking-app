032e1d301b99751bc9a269e0b29f8bf9c3f1543b
GIORGIgok <g.goqadze1@gmail.com>
2025-03-12 02:49:23 +0400

    created static markup of filtration

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -4,7 +4,7 @@ import "./styles/header-styles.css";
 
 export default function Header() {
   return (
-    <header className="w-full h-[100px] flex justify-between items-center px-[120px] py-[30px] mb-[30px]">
+    <header className="w-full h-[100px] flex justify-between items-center px-[120px] py-[30px] mb-[40px]">
       <figure>
         <Link to="/">
           <img src={logo_img} alt="logo" />
@@ -15,6 +15,7 @@ export default function Header() {
         <button className="button button-add-employee">
           თანამშრომლის შექმნა
         </button>
+
         <Link to="/add-new-task" className="button button-add-new-task">
           + შექმენი ახალი დავალება
         </Link>
--- src/pages/all-tasks-page/all-tasks-page.jsx
+++ src/pages/all-tasks-page/all-tasks-page.jsx
@@ -1,9 +1,30 @@
+import { ArrowDownSvg } from "../../assets/svg/arrow-down";
+
 export default function AllTasksPage() {
   return (
     <main className="px-[120px]">
       <h2 className="text-[34px] font-[FiraGO] font-bold text-[#212529]">
         დავალებების გვერდი
       </h2>
+      {/* SELECT DEPARTMENT */}
+      <div className="w-[688px] h-[44px] mt-[52px] border-1 border-[#DEE2E6] rounded-[10px] flex items-center gap-[45px]">
+        <div className="option flex justify-center items-center gap-[8px] w-[199px] hover:text-[#8338EC] hover:cursor-pointer group">
+          <span>დეპარტამენტი</span>
+          <ArrowDownSvg className="group-hover:fill-[#8338EC]" />
+        </div>
+
+        {/* SELECT PRIORITY */}
+        <div className="option flex justify-center items-center gap-[8px] w-[199px] hover:text-[#8338EC] hover:cursor-pointer group">
+          <span>პრიორიტეტი</span>
+          <ArrowDownSvg className="group-hover:fill-[#8338EC]" />
+        </div>
+
+        {/* SELECT EMPLOYEE */}
+        <div className="option flex justify-center items-center gap-[8px] w-[199px] hover:text-[#8338EC] hover:cursor-pointer group">
+          <span>თანამშრომელი</span>
+          <ArrowDownSvg className="group-hover:fill-[#8338EC]" />
+        </div>
+      </div>
     </main>
   );
 }
